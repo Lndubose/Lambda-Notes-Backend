731920c09a8550d12c61c2d79467d22dca631b16
lndubose <lndubose13@gmail.com>
2018-10-29 18:12:44 -0500

    Add cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const helmet = require('helmet');
+const cors = require('cors');
 const express = require('express');
 const Routes = require('./routes.js');
 
 const server = express();
 
-server.use(express.json(), helmet());
+server.use(express.json(), helmet(), cors());
 server.use('/api', Routes);
 
 module.exports = server;
