c6db4f2a6749efeaf5e650869ce0c9f714f30032
lndubose <lndubose13@gmail.com>
2018-10-30 17:22:38 -0500

    Heroku Development

--- knexfile.js
+++ knexfile.js
@@ -14,4 +14,17 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    useNullAsDefault: true,
+    ssl: true,
+  },
 };
