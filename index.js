1ad50f67d3e75fda400cbdd675f441169f023b37
lndubose <lndubose13@gmail.com>
2018-10-30 16:30:23 -0500

    Dev

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server = require('./api/server.js');
 const port = process.env.PORT || 7777;
 
+server.get('/', (req, res) => {
+  res.send('Running');
+});
+
 server.listen(port, () =>
   console.log(`===Server running on ${port} port===\n`)
 );
