e6878cee0b4cf661999ff0f235328519778ef9db
Lndubose <lndubose13@gmail.com>
2018-10-31 14:06:47 -0500

    Revert "Dev"
    This reverts commit 1ad50f67d3e75fda400cbdd675f441169f023b37.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const server = require('./api/server.js');
 const port = process.env.PORT || 7777;
 
-server.get('/', (req, res) => {
-  res.send('Running');
-});
-
 server.listen(port, () =>
   console.log(`===Server running on ${port} port===\n`)
 );
