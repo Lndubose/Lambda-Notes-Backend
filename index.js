445cb510c09b712cb9c83d594e280a474db6f55e
Lndubose <lndubose13@gmail.com>
2018-10-31 14:06:55 -0500

    Revert "Add port"
    This reverts commit 13d75e4f457deae1139b32952f7d7bffe55e7b46.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./api/server.js');
-const port = process.env.PORT || 7777;
+const port = 7777;
 
 server.listen(port, () =>
   console.log(`===Server running on ${port} port===\n`)
