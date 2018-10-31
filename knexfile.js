40b4a512842afd2a452782ebaf4a7b5c6c650177
Lndubose <lndubose13@gmail.com>
2018-10-31 14:06:52 -0500

    Revert "Add dev into knexfile.js"
    This reverts commit 3eeb06473c566480c135694da0602ebcc4818418.

--- knexfile.js
+++ knexfile.js
@@ -14,17 +14,4 @@ module.exports = {
       directory: './data/seeds',
     },
   },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-    useNullAsDefault: true,
-    ssl: true,
-  },
 };
