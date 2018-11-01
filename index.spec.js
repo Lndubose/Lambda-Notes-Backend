235ba3921b184b3fdd33c9318ed544ca19a657d6
lndubose <lndubose13@gmail.com>
2018-11-01 16:05:23 -0500

    Work on Tests
    Still working on test. I am having problems with migrations.

--- index.spec.js
+++ index.spec.js
@@ -5,12 +5,36 @@ const server = require('./api/server.js');
 const db = require('./data/config.js');
 
 describe('server', () => {
-  beforeEach(() => {
-    db.migrate.latest();
+  beforeAll(function(done) {
+    return db.migrate.latest().then(function() {
+      return db.seed.run().then(function() {
+        done();
+      });
+    });
   });
 
-  afterEach(() => {
-    db.migrate.rollback();
+  beforeEach(function(done) {
+    return db.migrate.rollback().then(function() {
+      return db.migrate.latest().then(function() {
+        return db.seed.run().then(function() {
+          done();
+        });
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    return db.migrate.rollback().then(function() {
+      done();
+    });
+  });
+
+  afterAll(function(done) {
+    return db.migrate.latest().then(function() {
+      return db.seed.run().then(function() {
+        done();
+      });
+    });
   });
 
   describe('GET /api/notes', () => {
@@ -19,11 +43,24 @@ describe('server', () => {
 
       expect(response.status).toBe(200);
     });
+
+    it('get an array of objects', async () => {
+      const response = await request(server).get('/api/notes');
+
+      expect(response.body).toBeGreaterThan(0);
+    });
+  });
+
+  describe('GET /api/notes/id', () => {
+    it('get a ', () => {});
   });
 
   describe('POST /api/notes', () => {
     it('if running correctly should get status 201(Created)', async () => {
-      const body = { title: 'hi', textBody: 'Howdy' };
+      const body = {
+        title: 'hi',
+        textBody: 'Howdy',
+      };
 
       const response = await request(server)
         .post('/api/notes')
@@ -45,6 +82,32 @@ describe('server', () => {
         .send(body);
 
       expect(response.body).toEqual(expect.objectContaining(expected));
+    });
+
+    describe('require title and textBody', () => {
+      it('Need title, get a 400(Bad Request) if not', async () => {
+        const body = {
+          textBody: 'Heya',
+        };
+
+        const response = await request(server)
+          .post('/api/notes')
+          .send(body);
+
+        expect(response.status).toBe(400);
+      });
+
+      it('Need textBody, get a 400(Bad Request) if not', async () => {
+        const body = {
+          title: 'Heya',
+        };
+
+        const response = await request(server)
+          .post('/api/notes')
+          .send(body);
+
+        expect(response.status).toBe(400);
+      });
     });
   });
 
