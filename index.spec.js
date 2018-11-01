b4fc8085c1f327774effaee6dd7d2f9748bd7384
lndubose <lndubose13@gmail.com>
2018-11-01 15:13:37 -0500

    Add POST test
    Added BeforeEach and AfterEach methods to the test cases.
    A test was added to POST to make sure that the message and id is being posted.

--- index.spec.js
+++ index.spec.js
@@ -2,11 +2,16 @@ const request = require('supertest');
 
 const server = require('./api/server.js');
 
+const db = require('./data/config.js');
+
 describe('server', () => {
-  // afterEach(() => {
-  //   server.db.dropDatabase();
-  //   console.log('runnning');
-  // });
+  beforeEach(() => {
+    db.migrate.latest();
+  });
+
+  afterEach(() => {
+    db.migrate.rollback();
+  });
 
   describe('GET /api/notes', () => {
     it('if running correctly should get status 200(Ok)', async () => {
@@ -19,11 +24,27 @@ describe('server', () => {
   describe('POST /api/notes', () => {
     it('if running correctly should get status 201(Created)', async () => {
       const body = { title: 'hi', textBody: 'Howdy' };
+
       const response = await request(server)
         .post('/api/notes')
         .send(body);
 
       expect(response.status).toBe(201);
+    });
+
+    it('posts the message and id when working', async () => {
+      const body = { title: 'hi', textBody: 'Howdy' };
+
+      const expected = {
+        message: 'Note created',
+        _id: expect.any(Number),
+      };
+
+      const response = await request(server)
+        .post('/api/notes')
+        .send(body);
+
+      expect(response.body).toEqual(expect.objectContaining(expected));
     });
   });
 
